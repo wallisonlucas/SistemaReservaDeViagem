c4139943ffc6d20de60886ff02eac316010feae4
Wallison Lucas <wallisonllucas@gmail.com>
2026-03-30 23:02:38 -0300

    Atividade Atualizada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import facade.ViagemFacade;
+import model.Cliente;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("------------------------------------------------------------");
+        ViagemFacade facade = new ViagemFacade();
+        Cliente cliente1 = new Cliente("João", "joao@email.com");
+        facade.reservasViagem(cliente1, "Recife");
+        System.out.println("------------------------------------------------------------");
+        Cliente cliente2 = new Cliente("Maria", "maria@email.com");
+        facade.reservasViagem(cliente2, "São Paulo");
+        System.out.println("------------------------------------------------------------");
     }
 }
